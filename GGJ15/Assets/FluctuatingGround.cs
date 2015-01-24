177fe92bf3a32e69689fc5582f8df9f8bee8d5ca
anilbey <tuncel.manil@gmail.com>
2015-01-24 15:07:00 +0200

    level design vol. 4

---
+++ GGJ15/Assets/FluctuatingGround.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FluctuatingGround : MonoBehaviour {
+
+	public float speed;
+	public bool yon;
+	// Use this for initialization
+	void Start () {
+		yon = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		this.transform.Translate(new Vector3(0, ((yon) ? 1 : -1) *speed*Time.deltaTime, 0));
+
+		if (yon == false && (this.transform.position.y <= -5))
+			yon = true;
+		else if (yon == true && this.transform.position.y >= 7.5)
+			yon = false;
+
+
+
+	}
+}
