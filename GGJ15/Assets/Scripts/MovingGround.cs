099324bafe52e10fee295d717a9d52c413a3af35
anilbey <tuncel.manil@gmail.com>
2015-01-24 14:44:27 +0200

    level design vol. 3
    level design and dynamics and physics

---
+++ GGJ15/Assets/Scripts/MovingGround.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingGround : MonoBehaviour {
+
+	public GameObject ground;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetMouseButton(0))
+		{
+			if (ground.transform.position.y > -2.3)
+				ground.transform.Translate(new Vector3(0,-5*Time.deltaTime,0));
+		}
+		else
+		{
+			if (ground.transform.position.y < 2.45)
+				ground.transform.Translate(new Vector3(0,5*Time.deltaTime,0));
+
+		}
+
+	}
+
+}
---
+++ GGJ15/Assets/Scripts/playAnim_v2.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playAnim_v2 : MonoBehaviour {
+
+	public string parameter = "playAnim";
+	public bool playOnClick, playOnCollision, playParent, playRandomly, playParentParent;
+	public float rangeMin = 5f;
+	public float rangeMax = 15f;
+	private bool playOnce = true;
+
+
+	void Start()
+	{
+		if(playRandomly && !playParent)
+		{
+			StopCoroutine("PlayWithDelay");
+			StartCoroutine("PlayWithDelay");
+		}
+	}
+
+
+	IEnumerator PlayWithDelay()
+	{
+		while(this.gameObject.activeSelf)
+		{
+			yield return new WaitForSeconds(Random.Range(rangeMin, rangeMax));
+			PlayAnimation();
+		}
+	}
+
+	void PlayAnimation()
+	{
+		if(playParent)
+			this.transform.parent.GetComponent<Animator>().SetBool(parameter,true);
+		else if (playParentParent)
+		{
+			this.transform.parent.parent.GetComponent<Animator>().SetBool(parameter,true);
+			print ("entered");
+		}
+		else
+			this.GetComponent<Animator>().SetBool(parameter,true);
+
+		Invoke("RevertParameter", 0.2f);
+	}
+
+	void RevertParameter()
+	{
+		if(playParent)
+			this.transform.parent.GetComponent<Animator>().SetBool(parameter,false);
+		else if(playParentParent)
+			this.transform.parent.parent.GetComponent<Animator>().SetBool(parameter,false);
+		else
+			this.GetComponent<Animator>().SetBool(parameter,false);
+	}
+
+
+	void OnMouseDown()
+	{
+
+		if(playOnce && playOnClick)
+		{
+			PlayAnimation();
+			playOnce = false;
+
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+		if(playOnCollision)
+			PlayAnimation();
+	}
+
+	void OnEnable()
+	{
+		if(playRandomly && !playParent)
+		{
+			StopCoroutine("PlayWithDelay");
+			StartCoroutine("PlayWithDelay");
+		}
+	}
+
+}
